f36fc65a457b43c67097c4d686a50b13d155820b
Jiří Šedý <jiri.sedy@metriky.cz>
2024-03-15 21:05:18 +0100

    feat: add new functionality + README.md

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,17 +12,28 @@ import (
 )
 
 func main() {
+	// Define command-line flags with specified names and defaults.
+	httpPort := flag.Int("http.port", 8080, "The port to run the Prometheus metrics server on")
+	refreshInterval := flag.Int("refresh", 600, "Interval between metric updates in seconds")
+	flag.Parse()
+
 	client := NewOpsgenieClient(os.Getenv("OPSGENIE_API_KEY"))
 
-	// Spuštění pravidelné aktualizace metrik v samostatné gorutině
+	// Convert refreshInterval to time.Duration for use with time.Sleep
+	updateInterval := time.Duration(*refreshInterval) * time.Second
+
+	// Start a separate goroutine for periodically updating metrics
 	go func() {
 		for {
+			log.Println("Starting metrics update")
 			updateMetrics(client)
-			time.Sleep(10 * time.Minute) // Aktualizace každých 10 minut, upravte podle potřeby
+			log.Println("Metrics update completed")
+			time.Sleep(updateInterval) // Wait for the specified interval before updating again
 		}
 	}()
 
-	// Nastavení HTTP serveru pro vystavení metrik Prometheus
+	// Set up the HTTP server to expose Prometheus metrics
 	http.Handle("/metrics", promhttp.Handler())
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Printf("Starting Prometheus metrics server on port %d\n", *httpPort)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *httpPort), nil))
 }
--- metricsCollector.go
+++ metricsCollector.go
@@ -8,31 +8,75 @@ import (
 )
 
 var (
-	// Definice metriky pro počet uživatelů
+	// Metric for tracking the last update timestamp of the Opsgenie data.
+	lastUpdateTimestamp = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "opsgenie_last_update_timestamp_seconds",
+		Help: "Timestamp of the last data update in Opsgenie exporter.",
+	})
+
+	// Metric for counting Opsgenie users by various characteristics such as total, blocked, and unverified.
 	opsgenieUsers = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "opsgenie_users",
 		Help: "Counts of Opsgenie users by various characteristics.",
 	}, []string{"key"})
-	// Definice metriky pro počet týmů
+
+	// Metric for tracking the verification status of Opsgenie users. Uses 0 for unverified, 1 for verified.
+	opsgenieUserVerifiedStatus = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "opsgenie_user_verified_status",
+		Help: "Verification status of Opsgenie users. 0 for unverified, 1 for verified.",
+	}, []string{"username"})
+
+	// Metric for counting the total number of Opsgenie teams.
 	opsgenieTeamsTotal = prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "opsgenie_teams_total",
 		Help: "Total number of Opsgenie teams.",
 	})
-	// Definice metriky pro účet
+
+	// Metric for displaying information about the Opsgenie account, such as user count, max user count, and yearly plan status.
 	opsgenieAccount = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "opsgenie_account",
 		Help: "Information about the Opsgenie account.",
 	}, []string{"key"})
+
+	// Metric for counting Opsgenie integrations by type.
+	opsgenieIntegrationsTotal = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "opsgenie_integrations_total",
+		Help: "Total number of Opsgenie integrations by type.",
+	}, []string{"type"})
+
+	// Metric for counting the total number of Opsgenie heartbeats.
+	opsgenieHeartbeatsTotal = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "opsgenie_heartbeats_total",
+		Help: "Total number of Opsgenie heartbeats.",
+	})
+
+	// Metric for counting the total number of enabled Opsgenie heartbeats.
+	opsgenieHeartbeatsEnabledTotal = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "opsgenie_heartbeats_enabled_total",
+		Help: "Total number of enabled Opsgenie heartbeats.",
+	})
+
+	// Metric for indicating whether an Opsgenie heartbeat is expired or not, labeled by team.
+	opsgenieHeartbeatsExpired = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "opsgenie_heartbeats_expired",
+		Help: "Indicates whether an Opsgenie heartbeat is expired (1) or not (0), labeled by team.",
+	}, []string{"team"})
 )
 
 func init() {
-	// Registrace metriky, aby Prometheus klient věděl, že ji má sbírat
+	// Register the metrics with Prometheus client to start collecting them.
+	prometheus.MustRegister(lastUpdateTimestamp)
 	prometheus.MustRegister(opsgenieUsers)
+	prometheus.MustRegister(opsgenieUserVerifiedStatus)
 	prometheus.MustRegister(opsgenieTeamsTotal)
 	prometheus.MustRegister(opsgenieAccount)
+	prometheus.MustRegister(opsgenieIntegrationsTotal)
+	prometheus.MustRegister(opsgenieHeartbeatsTotal)
+	prometheus.MustRegister(opsgenieHeartbeatsEnabledTotal)
+	prometheus.MustRegister(opsgenieHeartbeatsExpired)
 }
 
-// Pomocná funkce pro konverzi bool na float64, protože Prometheus metriky musí být číselné
+// Helper function to convert a bool to float64 since Prometheus metrics need to be numerical.
 func boolToFloat64(b bool) float64 {
 	if b {
 		return 1.0
@@ -40,13 +84,14 @@ func boolToFloat64(b bool) float64 {
 	return 0.0
 }
 
-// Funkce pro aktualizaci metriky uživatelů
+// updateMetrics concurrently updates various Prometheus metrics based on data fetched from Opsgenie.
 func updateMetrics(client *OpsgenieClient) {
 	var wg sync.WaitGroup
-	wg.Add(3) // Čekáme na dvě operace
+	wg.Add(5) // Waiting for five operations: users, teams, account, integrations, and heartbeats.
 
+	// Fetch and update user-related metrics in a goroutine.
 	go func() {
-		defer wg.Done()
+		defer wg.Done() // Ensure the WaitGroup counter decreases on goroutine completion.
 		users, err := client.ListUsers()
 		if err != nil {
 			log.Printf("Error fetching users: %v", err)
@@ -54,6 +99,7 @@ func updateMetrics(client *OpsgenieClient) {
 		}
 
 		var total, blocked, unverified int
+		// Iterate through users to count total, blocked, and unverified users.
 		for _, user := range users {
 			total++
 			if user.Blocked {
@@ -63,11 +109,22 @@ func updateMetrics(client *OpsgenieClient) {
 				unverified++
 			}
 		}
+		// Set the Prometheus gauge values for users metrics.
 		opsgenieUsers.With(prometheus.Labels{"key": "total"}).Set(float64(total))
 		opsgenieUsers.With(prometheus.Labels{"key": "blocked"}).Set(float64(blocked))
 		opsgenieUsers.With(prometheus.Labels{"key": "unverified"}).Set(float64(unverified))
+
+		// Additionally, set the verification status for each user.
+		for _, user := range users {
+			verifiedStatus := 1
+			if !user.Verified {
+				verifiedStatus = 0
+				opsgenieUserVerifiedStatus.WithLabelValues(user.Username).Set(float64(verifiedStatus))
+			}
+		}
 	}()
 
+	// Fetch and update teams-related metrics in a goroutine.
 	go func() {
 		defer wg.Done()
 		teams, err := client.ListTeams()
@@ -75,10 +132,11 @@ func updateMetrics(client *OpsgenieClient) {
 			log.Printf("Error fetching teams: %v", err)
 			return
 		}
+		// Set the total number of teams in a Prometheus gauge.
 		opsgenieTeamsTotal.Set(float64(len(teams)))
 	}()
 
-	// Paralelní získávání informací o účtu
+	// Concurrently fetch account information and update account-related metrics.
 	go func() {
 		defer wg.Done()
 		accountInfo, err := client.GetAccountInfo()
@@ -87,11 +145,70 @@ func updateMetrics(client *OpsgenieClient) {
 			return
 		}
 
-		// Nastavení metrik na základě získaných informací o účtu
+		// Set metrics based on the fetched account information.
 		opsgenieAccount.With(prometheus.Labels{"key": "userCount"}).Set(float64(accountInfo.Data.UserCount))
 		opsgenieAccount.With(prometheus.Labels{"key": "maxUserCount"}).Set(float64(accountInfo.Data.Plan.MaxUserCount))
 		opsgenieAccount.With(prometheus.Labels{"key": "isYearly"}).Set(boolToFloat64(accountInfo.Data.Plan.IsYearly))
 	}()
 
-	wg.Wait() // Počkáme, až obě Go rutiny dokončí
+	// Fetch and update integrations-related metrics in a goroutine.
+	go func() {
+		defer wg.Done()
+		integrations, err := client.ListIntegrations()
+		if err != nil {
+			log.Printf("Error fetching integrations: %v", err)
+			return
+		}
+		// Count and set the number of integrations by type.
+		typeCounts := make(map[string]int)
+		for _, integration := range integrations {
+			typeCounts[integration.Type]++
+		}
+		for integrationType, count := range typeCounts {
+			opsgenieIntegrationsTotal.WithLabelValues(integrationType).Set(float64(count))
+		}
+	}()
+
+	// Concurrently fetch heartbeats information and update heartbeats-related metrics.
+	go func() {
+		defer wg.Done()
+		heartbeats, err := client.ListHeartbeats()
+		if err != nil {
+			log.Printf("Error fetching heartbeats: %v", err)
+			return
+		}
+
+		var enabledCount, totalHeartbeats int
+		expiredAndEnabledCountByTeam := make(map[string]int)
+		// Iterate through heartbeats to count enabled and identify expired ones by team.
+		for _, hb := range heartbeats {
+			totalHeartbeats++
+			detail, err := client.GetHeartbeatDetail(hb.Name)
+			if err != nil {
+				log.Printf("Error fetching heartbeat detail for %s: %v", hb.Name, err)
+				continue
+			}
+			if detail.Enabled {
+				enabledCount++
+				if detail.Expired {
+					teamName := detail.OwnerTeam.Name
+					if teamName == "" {
+						teamName = "no_team" // Default value for heartbeats without a team.
+					}
+					expiredAndEnabledCountByTeam[teamName]++
+				}
+			}
+		}
+
+		// Update metrics for enabled and total heartbeats.
+		opsgenieHeartbeatsEnabledTotal.Set(float64(enabledCount))
+		// Update metrics for expired heartbeats by team.
+		for team, count := range expiredAndEnabledCountByTeam {
+			opsgenieHeartbeatsExpired.WithLabelValues(team).Set(float64(count))
+		}
+		opsgenieHeartbeatsTotal.Set(float64(totalHeartbeats))
+	}()
+
+	wg.Wait()                              // Wait for all goroutines to complete before updating the last update timestamp.
+	lastUpdateTimestamp.SetToCurrentTime() // Update the timestamp metric to the current time after metrics updates.
 }
